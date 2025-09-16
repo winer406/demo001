c86683e0e0ea48f579741180d7cc0e541858efcd
winer406 <107400957+winer406@users.noreply.github.com>
2025-09-16 21:24:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit as st
 
 st.title("Hello Streamlit-er 👋")
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
+if st.button("press me"):
+	st.write("you press me")
+else :
+	st.write("you don't press me")
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
-
-if st.button("Send balloons!"):
-    st.balloons()
